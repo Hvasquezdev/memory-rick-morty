7b0d4876bb2174b0a9d1461ef037570008f1e6fa
Hector Vazquez <hvasquezdev@gmail.com>
2023-05-12 19:29:09 -0300

    feat: add react router dom and his initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import './App.scss';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 import Header from './components/shared/header';
+import './App.scss';
+
 
 function App() {
   return (
     <div className='app'>
       <Header />
+
+      <main className="app__content">
+        <RouterProvider router={router} />
+      </main>
     </div>
   );
 }
