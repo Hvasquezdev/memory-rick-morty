8c6761d8c8792e0791ef3f3202e31642bc6057b5
Hector Vazquez <hvasquezdev@gmail.com>
2023-05-12 18:59:25 -0300

    feat: add new header component

---
+++ src/components/shared/header/index.tsx
@@ -0,0 +1,20 @@
+import rickAndMortyLogo from '../../../assets/img/ricky_morty_logo.png';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img
+        className='header__logo'
+        src={rickAndMortyLogo}
+        alt='Rick and Morty'
+        width={520}
+        height={180}
+      />
+
+      <h1 className='header__title'>Juego de memoria</h1>
+    </header>
+  );
+};
+
+export default Header;
