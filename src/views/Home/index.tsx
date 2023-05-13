578873f035a28e4dddb7e17176d2b63d0a520f38
Hector Vazquez <hvasquezdev@gmail.com>
2023-05-13 15:46:57 -0300

    feat(Home): show characters list

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,12 +1,23 @@
+import { useCharactersStore } from '../../store/useCharactersStore';
+import CharacterCard from '../../components/CharacterCard';
+import CharactersList from '../../components/CharactersList';
 import Title from '../../components/Title';
 import './Home.scss';
 
 const Home = () => {
+  const { characters } = useCharactersStore();
+
   return (
     <div className='home'>
-      <Title type='h2' size='lg'>Personajes</Title>
+      <Title type='h2' size='lg' className='home__title'>
+        Personajes
+      </Title>
 
-      Home page
+      <CharactersList>
+        {characters.map((character, key) => (
+          <CharacterCard character={character} key={key} isFlipped />
+        ))}
+      </CharactersList>
     </div>
   );
 };
