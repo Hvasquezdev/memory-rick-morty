3b07459502f819bd33934db0af8db1c166a5e3e2
Hector Vazquez <hvasquezdev@gmail.com>
2023-05-13 15:31:56 -0300

    feat(app): fetch random characters and save them in zustand store

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import { useCharactersRandomIds } from './hooks/useCharactersRandomIds';
+import { useCharactersByIds } from './hooks/useCharactersByIds';
+import { useCharactersStore } from './store/useCharactersStore';
 import Header from './components/shared/Header';
 import './App.scss';
 
 function App() {
+  const { ids } = useCharactersRandomIds();
+  const { characters } = useCharactersByIds(ids);
+  const { setCharacters } = useCharactersStore();
+
+  useEffect(() => {
+    if (characters.length) {
+      setCharacters([...characters, ...characters].sort((a, b) => a.id - b.id));
+    }
+  }, [characters, setCharacters]);
+
   return (
     <div className='app'>
       <Header />
