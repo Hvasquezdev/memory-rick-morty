243908ad89715326f6dd86900dd4136b786d1db7
Hector Vazquez <hvasquezdev@gmail.com>
2023-05-13 21:43:08 -0300

    feat(Board): add initial game behavior by using game manager hook

--- src/views/Board/index.tsx
+++ src/views/Board/index.tsx
@@ -1,9 +1,48 @@
-import "./Board.scss";
+import { useCallback, useEffect, useState } from 'react';
+import { useGameManager } from '../../hooks/useGameManager';
+import { useCharactersStore } from '../../store/useCharactersStore';
+import CharacterCard from '../../components/CharacterCard';
+import CharactersList from '../../components/CharactersList';
+import Title from '../../components/Title';
+import './Board.scss';
 
 const Board = () => {
+  const { characters } = useCharactersStore();
+  const { board, selectedIndex, matchedIndex, handleFlipCard, turnsPlayed, matchedCount } =
+    useGameManager(characters);
+  const [showCards, setShowCards] = useState(true);
+
+  const handleStartGame = useCallback(() => {
+    const shuffleTimeout = setTimeout(() => {
+      setShowCards(false);
+      clearTimeout(shuffleTimeout);
+    }, 3000);
+  }, []);
+
+  useEffect(() => handleStartGame, [handleStartGame]);
+
   return (
     <div className='board'>
-      <header className='board__header'>Board page</header>
+      <header className='board__header'>
+        <Title type='h2' size='lg' className='header-title'>
+          Aciertos: {matchedCount}
+        </Title>
+
+        <Title type='h2' size='lg' className='header-title'>
+          Turnos: {turnsPlayed}
+        </Title>
+      </header>
+
+      <CharactersList>
+        {board.map((character, index) => (
+          <CharacterCard
+            key={index}
+            character={character}
+            isFlipped={showCards || index in selectedIndex || index in matchedIndex}
+            onBackFaceClick={() => handleFlipCard(index)}
+          />
+        ))}
+      </CharactersList>
     </div>
   );
 };
