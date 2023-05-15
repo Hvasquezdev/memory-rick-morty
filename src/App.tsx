151c56fc59d2127163cb6c03629833fc91e99056
Hector Vazquez <hvasquezdev@gmail.com>
2023-05-15 00:10:40 -0300

    refactor(app): remove core data related behaviors

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import { useEffect } from 'react';
-import { useCharactersRandomIds } from './hooks/useCharactersRandomIds';
-import { useCharactersByIds } from './hooks/useCharactersByIds';
-import { useCharactersStore } from './store/useCharactersStore';
 import Router from './router';
 import Header from './components/shared/Header';
 import './App.scss';
 
 function App() {
-  const { ids } = useCharactersRandomIds();
-  const { characters } = useCharactersByIds(ids);
-  const { setCharacters, setLoadingCharacters } = useCharactersStore();
-
-  useEffect(() => {
-    if (characters.length) {
-      setCharacters([...characters, ...characters]);
-      setLoadingCharacters(false);
-    }
-  }, [characters, setCharacters, setLoadingCharacters]);
-
   return (
     <div className='app'>
       <Header />
