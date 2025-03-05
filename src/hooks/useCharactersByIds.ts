ae729543b71144675f8c41e0db10240ccf814abf
Hector Vasquez <hvasquezdev@gmail.com>
2025-03-05 10:00:28 -0300

    chore: change initial order of devsData const

--- src/hooks/useCharactersByIds.ts
+++ src/hooks/useCharactersByIds.ts
@@ -55,6 +55,11 @@ const devsData = [
     species: '',
   },
   {
+    name: 'Hector',
+    status: 'Frontend',
+    species: 'Veloz',
+  },
+  {
     name: 'Angel',
     status: 'Backend',
     species: 'La maquina troncoso',
@@ -62,11 +67,6 @@ const devsData = [
       start: '2025-02-05',
       end: '2025-02-12',
     },
-  },
-  {
-    name: 'Hector',
-    status: 'Frontend',
-    species: 'Veloz',
   },
   {
     name: 'Marquitos',
